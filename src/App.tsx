36f9c8e00d505388e1b30d243d6952a72dbc6ab6
Mauro Colella <mauro.ludovico.colella@gmail.com>
2020-05-10 20:14:42 +0800

    Add effect component + tune up

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ import { Canvas } from 'react-three-fiber';
 import Swarm from './components/Swarm';
 
 function App() {
-    const mouse = useRef([300, -200])
-    const onMouseMove = useCallback(({ clientX: x, clientY: y }) => (mouse.current = [x - window.innerWidth / 2, y - window.innerHeight / 2]), [])
+    const mouse = useRef([300, -200]);
+    const onMouseMove = useCallback(({ clientX: x, clientY: y }) => (mouse.current = [x - window.innerWidth / 2, y - window.innerHeight / 2]), []);
 
     return (
         <Canvas camera={{ fov: 45, position: [0, 0, 120] }} onMouseMove={onMouseMove}>
             <ambientLight />
             <pointLight position={[10, 10, 10]} />
-            <Swarm mouse={mouse} count={3600} />
+            <Swarm mouse={mouse} count={7200} />
         </Canvas>
     );
 }
---
+++ src/components/Effect/index.jsx
@@ -0,0 +1,25 @@
+import { Vector2 } from 'three';
+import React, { useEffect, useRef, useMemo } from 'react';
+import { extend, useFrame, useThree } from 'react-three-fiber';
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
+import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass';
+
+extend({ EffectComposer, ShaderPass, RenderPass, UnrealBloomPass });
+
+function Effect({ down }) {
+    const composer = useRef();
+    const { scene, gl, size, camera } = useThree()
+    const aspect = useMemo(() => new Vector2(size.width, size.height), [size])
+    useEffect(() => void composer.current.setSize(size.width, size.height), [size])
+    useFrame(() => composer.current.render(), 1)
+    return (
+        <effectComposer ref={composer} args={[gl]}>
+            <renderPass attachArray="passes" scene={scene} camera={camera} />
+            <unrealBloomPass attachArray="passes" args={[aspect, 0, 5, 1]} />
+        </effectComposer>
+    )
+}
+
+export default Effect;
