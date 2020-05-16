3992037968fe62af754709cc6c4dffd204c7c467
Mauro Colella <mauro.ludovico.colella@gmail.com>
2020-05-16 12:44:57 +0800

    Improve config

--- config-overrides.js
+++ config-overrides.js
@@ -1,14 +1,14 @@
 
 const { addWebpackPlugin, addWebpackAlias, override } = require('customize-cra');
-// const { addReactRefresh } = require('customize-cra-react-refresh');
+const { addReactRefresh } = require('customize-cra-react-refresh');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const path = require('path');
 
 module.exports = override(
   addWebpackAlias({
     three$: path.resolve('./src/utils/three.js'),
-    '../../../build/three.module.js': path.resolve('./src/utils/three.js')
+    '../../../build/three.module.js': path.resolve('./src/utils/three.js'),
   }),
-  // addReactRefresh(),
+  addReactRefresh({ disableRefreshCheck: true }),
   addWebpackPlugin(new BundleAnalyzerPlugin()),
 );
