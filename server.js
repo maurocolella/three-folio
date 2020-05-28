df37f9eac8e8bb8f7e2c1534c81baabc2ecb497b
Mauro Colella <mauro.ludovico.colella@gmail.com>
2020-05-29 02:43:41 +0800

    Enable compression

--- server.js
+++ server.js
@@ -3,8 +3,13 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
+const compression = require('compression');
+
 let app = express();
+
 app.use(express.static(path.join(__dirname, 'build')));
+app.use(compression());
+
 const port = process.env.PORT || '8080';
 app.set('port', port);
 const server = http.createServer(app);
